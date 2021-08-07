b0312c86d76938cef2da4e13be413d5ca7bcf24e
PatrykPrusko2019 <patrykprusko@gmail.com>
2021-08-07 09:03:21 +0200

    adds a selection dialog to the back button int class AddEditActivity

--- app/src/main/java/com/patrykprusko/tasktimer/AddEditActivity.java
+++ app/src/main/java/com/patrykprusko/tasktimer/AddEditActivity.java
@@ -6,10 +6,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
-public class AddEditActivity extends AppCompatActivity implements AddEditActivityFragment.OnSaveClicked {
+
+public class AddEditActivity extends AppCompatActivity implements AddEditActivityFragment.OnSaveClicked,
+                                    AppDialog.DialogEvents {
     private static final String TAG = "AddEditActivity";
-
+    public static final int DIALOG_ID_CANCEL_EDIT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,26 @@ public class AddEditActivity extends AppCompatActivity implements AddEditActivit
         fragmentTransaction.replace(R.id.fragment, fragment);
         fragmentTransaction.commit();
 
+    }
 
+    //dzieki temu dziala przycisk back button i wyswietla sie dialog wyboru
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d(TAG, "onOptionsItemSelected: home button pressed");
+                AddEditActivityFragment fragment = (AddEditActivityFragment)
+                        getSupportFragmentManager().findFragmentById(R.id.fragment);
 
+                if(fragment.canClose()) {
+                    return super.onOptionsItemSelected(item);
+                } else {
+                    showConfirmationDialog();
+                    return true; // indicate we are handling this
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 
@@ -41,4 +62,46 @@ public class AddEditActivity extends AppCompatActivity implements AddEditActivit
     public void onSaveClicked() {
         finish();
     }
+
+    @Override
+    public void onPositiveDialogResult(int dialogId, Bundle args) {
+        Log.d(TAG, "onPositiveDialogResult: called");
+    }
+
+    @Override
+    public void onNegativeDialogResult(int dialogId, Bundle args) {
+        Log.d(TAG, "onNegativeDialogResult: called");
+        finish();
+    }
+
+    @Override
+    public void onDialogCancelled(int dialogId) {
+        Log.d(TAG, "onDialogCancelled: called");
+    }
+
+    private void showConfirmationDialog() {
+        AppDialog dialog = new AppDialog();
+        Bundle args = new Bundle();
+        args.putInt(AppDialog.DIALOG_ID, DIALOG_ID_CANCEL_EDIT);
+        args.putString(AppDialog.DIALOG_MESSAGE, getString(R.string.cancelEditDiag_message));
+        args.putInt(AppDialog.DIALOG_POSITIVE_RID, R.string.cancelEditDiag_positive_caption);
+        args.putInt(AppDialog.DIALOG_NEGATIVE_RID, R.string.cancelEditDiag_negative_caption);
+
+        dialog.setArguments(args);
+        dialog.show(getSupportFragmentManager(), null);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Log.d(TAG, "onBackPressed: called");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        AddEditActivityFragment fragment = (AddEditActivityFragment) fragmentManager.findFragmentById(R.id.fragment); //zmieniamy na jaki korzysta content_main.xml w pozycji pionowej
+        if( fragment.canClose() ) {
+            super.onBackPressed();
+        } else {
+            showConfirmationDialog();
+        }
+    }
+
 }
